db46ae4c51efec2e5320dd5bd74166d470bc227e
happysmaran <smaranvallabhaneni@gmail.com>
2020-05-16 17:50:22 +0530

    nice

--- Bird.js
+++ Bird.js
@@ -1,7 +1,10 @@
 class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/bird.png");
+    this.image = loadImage("bird.png");
+    this.smokeImage = loadImage("smoke.png");
+
+    this.trajectory=[];
   }
 
   display() {
@@ -9,5 +12,12 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.position.x>220 && this.body.velocity.x>12){
+      var Position=[this.body.position.x, this.body.position.y];
+      this.trajectory.push(Position);
+    }
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var bird, slingshot;
 
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    backgroundImg = loadImage("bg.png");
 }
 
 function setup(){
